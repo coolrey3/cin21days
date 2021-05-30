27cd995b242d0c27a04ec39f00af864fe191b43a
Ray <coolrey3@gmail.com>
2021-05-29 23:03:52 -0400

    chapter 1 files

---
+++ Hello/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello World! This worked without the exclamation");
+    return 0;
+}
---
+++ bugBuster2/bug.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main( void)
+{
+    printf( "This is a program with a ");
+    printf( "problem! ");
+    return 0;
+
+}
