958cdca3b6c7c34c69504edee0e1314fd3d39ef9
Ray Mercedes <contact@RayMercedes.com>
2021-06-02 19:07:00 -0400

    update

--- PrintIt/print_it.c
+++ PrintIt/print_it.c
@@ -1,3 +1,13 @@
 /* This program prints a listing with line numbers! */
 #include <stdio.h>
 #include <stdlib.h>
+
+void do_heading(char *filename);
+
+int line = 0, page = 0;
+
+int main( int argv, char *argc[])
+{
+
+    
+}
